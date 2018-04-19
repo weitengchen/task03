3204fe47801d76b5becbee24dde4d57bbb3a1564
陳為騰 <yingpppppo@gmail.com>
2018-04-19 16:36:35 +0800

    Initial Commit

---
+++ task03Tests/task03Tests.swift
@@ -0,0 +1,36 @@
+//
+//  task03Tests.swift
+//  task03Tests
+//
+//  Created by WEI-TENG CHEN on 2018/4/19.
+//  Copyright © 2018年 WEI-TENG CHEN. All rights reserved.
+//
+
+import XCTest
+@testable import task03
+
+class task03Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
